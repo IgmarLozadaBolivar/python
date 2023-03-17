9e9c49518dea1c313deb6d260f84b847df4514ac
Igmar De Jesus Lozada Bolivar <94941937+WingDark@users.noreply.github.com>
2023-03-17 12:31:49 -0500

    Operadores Lógicos

---
+++ operadoresLogicos.py
@@ -0,0 +1,30 @@
+#Operadores Logicos
+
+#And
+"""
+True, True = True
+True, False = False
+False, True = False
+False, False = False
+"""
+
+#Or
+"""
+True, True = True
+True, False = True
+False, True = True
+False, False = False
+"""
+
+#Not
+"""
+True/False
+False/True
+"""
+a = True
+b = False
+
+print(a and b)
+print(a or b)
+print(not a)
+print(not b)
