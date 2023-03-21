b05c149ae6fd452a7f7d648bd8b3c668e391b591
Igmar De Jesus Lozada Bolivar <94941937+WingDark@users.noreply.github.com>
2023-03-21 15:25:03 -0500

    Ejercicios de condicionales

---
+++ exercise_Condicionales.py
@@ -0,0 +1,37 @@
+#Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no
+
+'''print("\n\n\n")
+print("********** ¿ARE YOU OLDER OR YOUNGER? ***********\n")
+age = int(input("Enter your age: "))
+
+if age < 18:
+    print("You are", age, "years old, you are a minor")
+else:
+    print("You are", age, "years old, you are of legal age")'''
+
+#Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar
+
+'''print("\n\n\n")
+print("********** ODD AND EVEN NUMBERS CALCULATOR **********\n")
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print(number, "is pair")
+else: 
+    print(number, "is odd")'''
+
+#Escribir un programa para una empresa que tiene salas de juegos para todas las edades y quiere calcular
+#de forma automática el precio que debe cobrar a sus clientes por entrar. El programa debe preguntar al usuario 
+#la edad del cliente y mostrar el precio de la entrada. Si el cliente es menor de 4 años puede entrar gratis, 
+#si tiene entre 4 y 18 años debe pagar 5€ y si es mayor de 18 años, 10€ """
+
+'''print("\n\n\n")
+print("********** ENTRY CALCULATION SYSTEM BY AGE **********\n")
+customer_age = int(input("Enter your age, please: "))
+if customer_age < 4:
+    price = 0
+elif customer_age < 17:
+    price = 5
+else:
+    price = 10
+print("The entrance price is: ", price, "€")'''
