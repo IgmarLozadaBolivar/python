66a054c4f2504493b182e37ae6bd3315a670e0c9
Igmar De Jesus Lozada Bolivar <94941937+WingDark@users.noreply.github.com>
2023-03-21 21:31:47 -0500

    Ciclo For

---
+++ classFor.py
@@ -0,0 +1,12 @@
+#personas = ["Juan Manuel", "Maria Paula", "Lina Maria", "Jhon Ceballos"]
+num =  " 2"  " 8"  " 9"  " 8"
+num2 = " 5"  " 8"  " 7"  " 5"
+num3 =  " 2"  " 8"  " 9"  " 6"
+#print("\n\n\n")
+#for i in range(4):
+    #print(personas[i])  
+for i in range(1):
+    print(num)
+    print(num2)
+    print(num3)
+
