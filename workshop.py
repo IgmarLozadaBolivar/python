d7fd9eca859f301ce0eceacdc381464ae1cc5ee1
Igmar De Jesus Lozada Bolivar <94941937+WingDark@users.noreply.github.com>
2023-03-27 15:11:30 -0500

    Taller en Español

--- workshop.py
+++ workshop.py
@@ -5,35 +5,31 @@ ingresado sea 99999. Una vez creada la lista, se desea conocer cuales y cuántos
 son pares e impares.
 '''
 
-'''
-Ejercicio utilizando diccionarios, listas, bucles como while y for y utilizando condicionales 
-'''
-
-#Initialize an empty dictionary to store even and odd counts (Dictionary)
+#Inicializar un diccionario vacío para almacenar conteos pares e impares (Dictionary)
 numCounts = {
     "even": 0,
     "odd": 0
 }
 
-#Initialize an empty list to store the numbers (List)
+#Inicializar una lista vacía para almacenar los números (List)
 num_list = []
 
-#Keep accepting integers until 99999 is entered (Loop while, function append, break and conditionals)
+#Continúe aceptando números enteros hasta que se ingrese 99999 (Loop while, method append, break y conditionals)
 while True:
-    num = int(input("Enter a number until you enter 99999 to finish: "))
+    num = int(input("Ingrese un número hasta ingresar 99999 para finalizar: "))
     if num == 99999:
         break
     num_list.append(num)
 
-#Count the number of even and odd integers (Loop for and conditionals)
+#Contar el número de enteros pares e impares (Loop for y conditionals)
 for num in num_list:
     if num % 2 == 0:
         numCounts["even"] += 1
     else:
         numCounts["odd"] += 1
 
-#Print the results (Print)
-print("There are", numCounts["even"], "even numbers and", numCounts["odd"], "odd numbers in the list.")
+#Imprimir los resultados (Imprimir)
+print("Hay", numCounts["even"], "números pares y", numCounts["odd"], "números impares en la lista.")
 
 '''
 Ejercicio utilizando diccionarios, listas, bucle while y utilizando condicionales 
@@ -41,23 +37,23 @@ Ejercicio utilizando diccionarios, listas, bucle while y utilizando condicionale
 
 from collections import Counter
 
-#Initialize an empty list to store the numbers (List)
+#Inicializar una lista vacía para almacenar los números (List)
 num_list = []
 
-#Keep accepting integers until 99999 is entered (Loop while and conditionals)
+#Continúe aceptando números enteros hasta que se ingrese 99999 (Loop while y conditionals)
 while True:
-    num = int(input("Enter an integer (99999 to stop): "))
+    num = int(input("Introduzca un número entero (99999 para detener): "))
     if num == 99999:
         break
     num_list.append(num)
 
-#Count even and odd numbers using Counter (Function counter and lambda)
+#Cuente números pares e impares usando Counter (Function counter and lambda)
 num_counts = Counter(num_list)
 even_count = num_counts[lambda x: x % 2 == 0]
 odd_count = num_counts[lambda x: x % 2 != 0]
 
-#Print the results (Print)
-print("There are", even_count, "even numbers and", odd_count, "odd numbers in the list.")
+#Imprimir los resultados (Print)
+print("Hay", even_count, "números pares y", odd_count, "números impares en la lista.")
 
 '''
 Dada una lista con nombres completos de personas, realizar un programa que
@@ -68,35 +64,31 @@ Se debe imprimir la lista de nombres y la lista con la cantidad de palabras de c
 nombre.
 '''
 
-'''
-Ejercicio utilizando diccionarios, listas, bucles como while, for y utilizando condicionales
-'''
-
-#Initialize an empty list to store fully qualified names (List)
+#Inicializar una lista vacía para almacenar nombres completos (List)
 name_list = []
 
-#Keep accepting names until "END" is entered (Loop while, function append, break and conditionals)
+#Continúe aceptando nombres hasta que ingrese "FIN" (Loop while, function append, break and conditionals)
 while True:
-    name = input("Type a full name to finish typing END: ")
-    if name == "END":
+    name = input("Escriba un nombre completo para terminar de escribir FIN: ")
+    if name == "FIN":
         break
     name_list.append(name)
 
-#Create a dictionary to store the number of words in each name (Dictionary)
+#Crear un diccionario para almacenar la cantidad de palabras en cada nombre (Dictionary)
 word_counts = {}
 
-#Iterate through the names in the list and count the words (Loop for, function split and len)
+#Iterar a través de los nombres en la lista y contar las palabras (Loop for, function split and len)
 for name in name_list:
-    words = name.split()  # Divide
-    num_words = len(words)  # Cuenta
-    word_counts[name] = num_words  # Almacena
+    words = name.split()
+    num_words = len(words)
+    word_counts[name] = num_words
 
-#Print the results (Print and loop for)
-print("List of names:")
+#Imprime los resultados (Print and loop for)
+print("Lista de nombres:")
 for name in name_list:
     print(name)
 print()
-print("Number of words in each name:")
+print("Número de palabras en cada nombre:")
 for name, count in word_counts.items():
     print(name, "-", count)
 
@@ -114,40 +106,40 @@ Para aprobar, la nota deber ser mayor o igual a 3.0 y la información en su tota
 en listas.
 '''
 
-#Function to calculate the final grade of a student (Function and return)
+#Función para calcular la nota final de un alumno (Function and return)
 def calculateNote(n1, n2, n3):
     nF = n1 * 0.3 + n2 * 0.3 + n3 * 0.4
     return nF
 
-#List of dictionaries to store student information (List)
+#Lista de diccionarios para almacenar información de los estudiantes (List)
 students = []
 
-#Cycle to capture student data (Loop while, function append, break and conditionals)
+#Ciclo para capturar datos de estudiantes (Loop while, function append, break and conditionals)
 while True:
-    StudentCode = int(input("Enter student code to finish, enter 999: "))
+    StudentCode = int(input("Ingrese el código de estudiante para terminar, ingrese 999: "))
     if StudentCode == 999:
         break
-    name = input("Enter student name: ")
-    n1 = float(input("Enter the student's note 1: "))
-    n2 = float(input("Enter the student's note 1: "))
-    n3 = float(input("Enter the student's note 1: "))
+    name = input("Ingrese el nombre del estudiante: ")
+    n1 = float(input("Ingrese la nota 1 del estudiante: "))
+    n2 = float(input("Ingrese la nota 2 del estudiante: "))
+    n3 = float(input("Ingrese la nota 3 del estudiante: "))
     nF = calculateNote(n1, n2, n3)
     approved = nF >= 3.0
-    student = {"Student Code": StudentCode, "Name": name, "note1": n1, "note2": n2, "note3": n3, "finalNote": nF, "approved": approved}
+    student = {"Código estudiantil": StudentCode, "Nombre": name, "Nota 1": n1, "Nota 2": n2, "Nota 3": n3, "Nota Definitiva": nF, "Aprobacion": approved}
     students.append(student)
 
-#Printing student information (Print's, loop for and conditionals)
+#Imprimir información del estudiante (Print's, loop for and conditionals)
 for student in students:
-    print("Student Code:", student["Student Code"])
-    print("Name:", student["nombre"])
-    print("Note 1:", student["note1"])
-    print("Note 2:", student["note2"])
-    print("Note 3:", student["note3"])
-    print("Definitive Note:", student["finalNote"])
-    if student["approved"]:
-        print("State: Approved")
+    print("Código estudiantil:", student["Código estudiantil"])
+    print("Nombre:", student["Nombre"])
+    print("Nota 1:", student["Nota 1"])
+    print("Nota 2:", student["Nota 2"])
+    print("Nota 3:", student["Nota 3"])
+    print("Nota definitiva:", student["Nota Definitiva"])
+    if student["Aprobacion"]:
+        print("Estado: Aprobado")
     else:
-        print("State: Reproached")
+        print("Estado: Reprobado")
     print()
 
 '''
@@ -160,35 +152,35 @@ Se suministra el diccionario de nombres de artículo y otro con los valores unit
 • Valores = {1:2500,2:3800,3:1200,4:35000,5:3700}.
 '''
 
-#Dictionary list to store the information of purchased items (Dictionary's)
-articles = {1:"Pencil", 2:"Notebooks", 3:"Draft", 4:"Calculator", 5:"Square"}
+#Lista de diccionarios para almacenar la información de los artículos comprados (Dictionary's)
+articles = {1:"Lapiz", 2:"Cuadernos", 3:"Borrador", 4:"Calculadora", 5:"Escuadra"}
 values = {1:2500, 2:3800, 3:1200, 4:35000, 5:3700}
 
-#Dictionary list to store the information of purchased items (list)
+#Lista de diccionarios para almacenar la información de los artículos comprados (list)
 purchase = []
 
-#Cycle to capture data from purchased items (Loop while, function append, break and conditionals)
+#Ciclo para capturar datos de artículos comprados (Loop while, function append, break and conditionals)
 while True:
-    itemCode = int(input("Enter the item code, to finish enter 0: "))
+    itemCode = int(input("Introduce el código del artículo, para finalizar introduce 0: "))
     if itemCode == 0:
         break
-    quantity = int(input("Enter the number of units purchased: "))
+    quantity = int(input("Ingrese el número de unidades compradas: "))
     name = articles[itemCode]
     unitValue = values[itemCode]
     totalValue = quantity * unitValue
-    articles = {"itemCode": itemCode, "Name": name, "Quantity": quantity, "unitValue": unitValue, "totalValue": totalValue}
+    articles = {"Codigo Del Articulo": itemCode, "Nombre": name, "Cantidad": quantity, "Valor Unitario": unitValue, "Valor Total": totalValue}
     purchase.append(articles)
 
-#Printing the information of the purchased items (Print's, loop for)
+#Impresión de la información de los artículos comprados (Print's, loop for)
 print("Purchase made:")
 for articles in purchase:
-    print("Item Code:", articles["itemCode"])
-    print("Name:", articles["Name"])
-    print("Quantity:", articles["Quantity"])
-    print("Unit Value:", articles["unitValue"])
-    print("Total Value:", articles["totalValue"])
+    print("Codigo del articulo:", articles["Codigo Del Articulo"])
+    print("Nombre del articulo:", articles["Nombre"])
+    print("Cantidad:", articles["Cantidad"])
+    print("Valor unitario:", articles["Valor Unitario"])
+    print("Valor total:", articles["Valor Total"])
     print()
 
-#Calculation of the total purchase value (Print)
-totalPurchaseValue = sum(articles["totalValue"] for articles in purchase)
-print("Total Purchase Value:", totalPurchaseValue)
+#Cálculo del valor total de compra (Print)
+totalPurchaseValue = sum(articles["Valor Total"] for articles in purchase)
+print("Valor total de la compra:", totalPurchaseValue)
