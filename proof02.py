755b21d693dbece386db8c97959cbdb956c4bf86
Igmar De Jesus Lozada Bolivar <94941937+WingDark@users.noreply.github.com>
2023-03-27 09:01:04 -0500

    proof2

--- proof02.py
+++ proof02.py
@@ -1,108 +1,129 @@
 ###IGMAR DE JESUS LOZADA BOLIVAR###
 ###GROUP W04###
-'''Crea una aplicación que contenga todas las siguientes funcionalidades a las cuales se debe 
-acceder por medio de un menú:
+'''Crea una aplicación que contenga todas las siguientes funcionalidades a las cuales se debe acceder por medio de un menú:
 '''
-
-'''
-1.Crea una aplicación que nos calcule el área de un círculo, cuadrado o triangulo.
-Pediremos qué figura queremos calcular, su área y según lo introducido pedirá los valores
-necesarios para calcular el área. Crea una función por cada figura para calcular cada área,
-este devolverá un número real. Muestra el resultado por pantalla
-    Circulo: (radio^2)*PI
-    Triangulo: (base * altura) / 2
-    Cuadrado: lado * lado
-'''
-    
+#Area figures
 import math
 
-def calculateAreaCircle():
-    radio = float(input("Enter the radius of the circle: "))
-    area = math.pi * radio ** 2
-    return area
-def calculateTriangleArea():
-    base = float(input("Enter the base of the triangle: "))
-    height= float(input("Enter the height of the triangle: "))
-    area = (base * height) / 2
-    return area
-def calculateSquareArea():
-    side = float(input("Enter from the side of the square: "))
-    area = side ** 2
-    return area
+def calculateAreaCircle(radio):
+        area = (radio**2)*3.1416
+        return area
+def calculateTriangleArea(base, height):
+        area = (base * height)/2
+        return area
+def calculateSquareArea(side):
+        area = (side * side)
+        return area
 
-figure = input("¿What shape do you want to calculate the area of?:\n (Circle)\n (Triangle)\n (Square)\n")
-if figure == 'Circle':
-    area = calculateAreaCircle()
-elif figure == 'Triangle':
-    area = calculateTriangleArea()
-elif figure == 'Square':
-    area = calculateSquareArea()
-else: 
-    print("Select an option")
-    area = 0
-
-print(f"The area of {figure} is {area}")
-
-'''
-2.Crea una aplicación que nos pida un número por teclado y con una función se lo pasamos
-por parámetro para que nos indique si es o no un número primo, debe devolver true si
-es primo sino false.
-Un número primo es aquel solo puede dividirse entre 1 y si mismo.
-Por ejemplo: 25 no es primo, ya que 25 es divisible entre 5, sin embargo, 17 si es primo.
-'''
-
-def isPrime(number):
-        if number < 2:
+#Prime number
+def isPrime(num):
+    for n in range(2, num):
+        if num % n == 0:
             return False
-        for i in range(2, int(number ** 0.5) + 1):
-            if number % 1 == 0:
-                return False
         return True
 
-number = int(input("Enter a number: "))
-if isPrime(number):
-    print(f"{number} is a prime number")
-else:
-    print(f"{number} is not a prime number")
+#Factorial number
+def factorialNumber (num):
+        if num == 0 or num == 1:
+          resultado = 1
+        elif num > 1:
+             resultado = num*factorialNumber(num-1)
+        return resultado
 
-'''
-3.Crea una aplicación que nos calcule el factorial de un número pedido por teclado, lo
-realizara mediante una función al que le pasamos el número como parámetro. Para calcular el
-multiplica los números anteriores hasta llegar a uno. Por ejemplo, si introducimos un 5,
-realizara esta operación 5*4*3*2*1=120.
-'''
+#Currency exchange
+def currencyConverter(quantity, currency):
+    usd_rate = 3000
+    jpy_rate = 28.5
+    gbp_rate = 4100
 
-'''
-4.Crea una aplicación que nos convierta una cantidad de pesos introducida por teclado a otra
-moneda, estas pueden ser a dolares, yenes o libras. La función tendrá como parámetros, la
-cantidad de pesos y la moneda a pasar que será un texto
-'''
+    if currency == "dollars":
+        converted_amount = quantity / usd_rate
+        return f"${converted_amount:.2f}"
+    elif currency == "yen":
+        converted_amount = quantity / jpy_rate
+        return f"¥{converted_amount:.2f}"
+    elif currency == "libras":
+        converted_amount = quantity / gbp_rate
+        return f"£{converted_amount:.2f}"
+    else:
+        return "Moneda no soportada"
+#
+def calculateQuadraticEquation(a, b, c):
+    discriminant = b**2 - 4*a*c
+    if discriminant < 0:
+        print('¡The equation has no real solutions!')
+        return None
+    else:
+        x1 = (-b + math.sqrt(discriminant)) / (2*a)
+        x2 = (-b - math.sqrt(discriminant)) / (2*a)
+        return x1, x2
+    
+while True:
+    print("MENU")
+    print("Select an option: ")
+    print("1.Calculate figures")
+    print("2.Calculate the prime number")
+    print("3.Calculate the factorial number")
+    print("4.Convert pesos to another currency")
+    print("5.Solving a quadratic equation")
+    print("6. Exit")
+    option = input()
+    
+    if option == "1":
+        print("Select the figure you want to calculate ")
+        print("1.Circle")
+        print("2.Triangle")
+        print("3.Square")
+        figure = input("Ingresa el numero de la figura que deseas realizar: ")
+        if figure == "1":
+            radio = float(input("Enter the radius of the circle: "))
+            print("The area of ​​the circle is: ", calculateAreaCircle(radio))
+        elif figure == "2":
+            base = int(input("Enter the base of the triangle: "))
+            height = int(input("Enter the height of the triangle: "))
+            print("The area of ​​the triangle is: ", calculateTriangleArea(base,height))
+        else:
+            side = int(input("Enter from the side of the square: "))
+            print("The area of ​​the square is: ", calculateSquareArea(side))
+
+        options = input("¿Do you want to select another calculator? s/n\n")
 
 
-def currencyConverter(pesos, currency):
-   if currency == 'Dollars':
-       exchangeRate = 0.00028
-       conversion = pesos * exchangeRate
-       return conversion
-   elif currency ==  'Yen':
-       exchangeRate = 0.030
-       conversion = pesos * exchangeRate
-   elif currency == 'Pounds':
-       exchangeRate = 0.00020
-       conversion = pesos * exchangeRate
-   else:
-       return "Invalid currency"
+    elif option == "2":
 
-quantity = float(input("Enter the amount of pesos you want to convert: "))
-currencyDestiny = input("Enter the destination currency (Dollars, Yen to Pounds): ")
-result = currencyConverter(quantity, currencyDestiny)
-if isinstance(result, str):
-    print(result)
-else:
-    print(f"{quantity} pesos equivale a {result} {currencyDestiny}")
+        while options.upper() == "S":
+            num = int(input("Enter a number: "))
+            print("¿Is the entered number prime? ",isPrime(num))
 
-'''
-5.Crea una aplicación que replique la ecuasión cuadrática, solicitando los valores para
-a, b y c, y exponiendo los 2 resultados que esta genera. Tener en cuenta que se debe validar 
-si los valores dados por el usuario generan un resultado imaginario, de ser así debe informarse
+            options = input("¿Do you want to enter another number? s/n\n")
+
+    elif option == "3":
+         
+        while options.upper() == "S":
+            num = int(input("Enter a number: "))
+            print("The result of the factorial number is: ", factorialNumber(num))
+
+            options = input("¿Do you want to enter another number? s/n\n")
+
+    elif option == '4':
+        quantity = float(input('Enter the amount of pesos: '))
+        currency = input('Enter the currency you want to convert to (dollars, yen or pounds): ')
+        result = currencyConverter(quantity, currency)
+        if result is not None:
+            print(f'{quantity} weights is equivalent to {result} {currency}.')
+    
+    elif option == '5':
+        a = float(input('Enter the coefficient a: '))
+        b = float(input('Enter the coefficient b: '))
+        c = float(input('Enter the coefficient c: '))
+        result = calculateQuadraticEquation(a, b, c)
+        if result is not None:
+            print(f'The results are: {result[0]} y {result[1]}.')
+    
+    elif option == '6':
+        print('¡See you later!')
+        break
+    
+    else:
+        print('Invalid option, try again.')
 '''
