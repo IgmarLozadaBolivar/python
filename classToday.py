f1b7d0daee2fb2c40b6d69fac8da9166729761cc
Igmar De Jesus Lozada Bolivar <94941937+WingDark@users.noreply.github.com>
2023-03-30 11:07:21 -0500

    classToday

--- classToday.py
+++ classToday.py
@@ -2,29 +2,36 @@ print("\n\n")
 import os
 
 dicNotas = {
-
+    0: {
+    #"codigo": "",
+    "nota1": "",
+    "nota2": "",
+    "nota3": "",
+    "nota4": "",
+    "nota5": "",
+    }
 }
 
 dicEstudiantes = {
     1: {
     "nombre": "Juan",
     "apellido": "Garcia",
-    "correo": "juan@gmail.com"
+    "correo": "juan@gmail.com",
     },
     2: {
     "nombre": "Adrian",
     "apellido": "Martinez",
-    "correo": "adrian@gmail.com"
+    "correo": "adrian@gmail.com",
     },
     3: {
     "nombre": "Laura",
     "apellido": "Lopez",
-    "correo": "laura@gmail.com"
+    "correo": "laura@gmail.com",
     },
     4: {
     "nombre": "Jessica",
     "apellido": "Muñoz",
-    "correo": "jessica@gmail.com"
+    "correo": "jessica@gmail.com",
     }
 }
 
@@ -54,12 +61,12 @@ def verMenu():
 def verMenuNotas(notas):
     os.system("clear")
     print("********* NOTAS *********\n")
-    print("CODIGO\t\tNOTA")
+    print("CODIGO\t\t\tNOTA 1\t\t\tNOTA 2\t\t\tNOTA 3")
     for nota in notas:
-        print(nota,"\t\t\t",notas[nota]["nombre"])
+        print(nota,"\t\t\t",notas[nota]["nota1"],"\t\t\t",notas[nota]["nota2"],"\t\t\t",notas[nota]["nota3"])
     print('''Seleccione una de las siguientes opciones:
-    1. Agregar        2. Editar
-    3. Eliminar     0. Salir''')
+    1. Agregar nota         2. Editar nota
+    3. Eliminar nota        0. Salir''')
 
 #           ESTUDIANTES
 def verMenuEstudiantes(estudiantes):
@@ -67,21 +74,23 @@ def verMenuEstudiantes(estudiantes):
     print("********* ESTUDIANTES *********\n")
     print("CODIGO\t\t\tNOMBRE\t\t\tAPELLIDO\t\t\tCORREO")
     for estudiante in estudiantes:
-        print(estudiante,"\t\t\t",estudiantes[estudiante]["nombre"],"\t\t\t",estudiantes[estudiante]["apellido"],"\t\t\t",estudiantes[estudiante]["correo"])
+        print(estudiante,"\t\t\t",estudiantes[estudiante][f"nombre"],"\t\t\t",estudiantes[estudiante][f"apellido"],"\t\t\t",estudiantes[estudiante][f"correo"])
     print('''Seleccione una de las siguientes opciones: 
-    1. Agregar      2. Editar
-    3. Eliminar     0. Salir''')
+    1. Agregar estudiante       2. Editar estudiante
+    3. Eliminar estudiante      0. Salir''')
 
 #           MATERIAS
 def verMenuMaterias(materias):
     os.system("clear") 
     print("********* MATERIAS *********\n")
-    print("CÓDIGO\t\tMATERIA")
+    print(f'{"CÓDIGO":<25} {"MATERIA"}')
+          #{"\t\t\t"},{"MATERIA"}')
+    print(f'{"------":<25} {"-------------"}')
     for materia in materias:
         print(materia,"\t\t\t",materias[materia]["nombre"])
     print('''Seleccione una de las siguientes opciones: 
-    1. Crear        2. Editar
-    3. Eliminar     0. Salir''')
+    1. Crear materia        2. Editar materia
+    3. Eliminar materia     0. Salir''')
 
 verMenu()
 opcMenu = input()
@@ -93,6 +102,44 @@ while opcMenu != "0":
         while opc != "0":
             if opc == "1":
                 print("********* AGREGAR *********")
+                id = list(dicNotas.keys())[len(dicNotas.keys())-1]+1
+                print("Por favor ingrese el codigo a agregar: ")
+                codigo = input()
+                print("Por favor ingrese la nota 1: ")
+                nota1 = float(input())
+                print("Nota agregada con exito")
+                print("Por favor ingrese la nota 2: ")
+                nota2 = float(input())
+                print("Nota agregada con exito")
+                print("Por favor ingrese la nota 3: ")
+                nota3 = float(input())
+                print("Nota agrga con exito")
+                dicNotas[id] = {"nota1": nota1, "nota2": nota2, "nota3": nota3}
+            elif opc == "2":
+                print("********* EDITAR *********")
+                print("Por favor ingrese el código de la nota: ")
+                id = int(input())
+                print("¡Codigo ingresado!")
+                print("Por favor ingrese la nueva nota: ")
+                nota1 = float(input())
+                dicNotas[id]["nota1"] = nota1
+                print("Nota editada con exito")
+                print("Por favor ingrese la nueva nota: ")
+                nota2 = float(input())
+                dicNotas[id]["nota2"] = nota2
+                print("Nota editada con exito")
+                print("Por favor ingrese la nueva nota: ")
+                nota3 = float(input())
+                dicNotas[id]["nota3"] = nota3
+                print("Nota editada con exito")
+            elif opc == "3":
+                print("********* ELIMINAR *********")
+                print("Por favor ingrese el código de la nota: ")
+                id = int(input())
+                del dicNotas[id]
+                print("Notas eliminadas con exito")
+            verMenuNotas(dicNotas)
+            opc = input()
     elif opcMenu == "2":
         print("******** ESTUDIANTES ********\n")
         verMenuEstudiantes(dicEstudiantes)
@@ -103,10 +150,13 @@ while opcMenu != "0":
                 id = list(dicEstudiantes.keys())[len(dicEstudiantes.keys())-1]+1
                 print("Por favor ingrese el nombre del estudiante: ")
                 nombre = input()
+                print("Nombre agregado con exito")
                 print("Por favor ingrese el apellido del estudiante: ")
                 apellido = input()
+                print("Apellido agregado con exito")
                 print("Por favor ingrese el correo del estudiante: ")
                 correo = input()
+                print("Correo agregado con exito")
                 dicEstudiantes[id] = {"nombre": nombre, "apellido": apellido, "correo": correo}
                 """ dicEstudiantes[id] = {"nombre": nombre}
                 print("Por favor ingrese el apellido del estudiante: ")
@@ -121,18 +171,22 @@ while opcMenu != "0":
                 print("********* EDITAR *********\n")
                 print("Por favor ingrese el código del estudiante a editar: ")
                 id = int(input())
+                print("¡Codigo ingresado!")
                 print("Por favor ingrese el nombre del nuevo estudiante: ")
                 dicEstudiantes[id]["nombre"] = input()
+                print("¡Nombre editado con exito!")
                 print("Por favor ingrese el apellido del estudiante: ")
                 dicEstudiantes[id]["apellido"] = input()
+                print("¡Apellido editado con exito!")
                 print("Por favor ingrese el correo del estudiante: ")
                 dicEstudiantes[id]["correo"] = input()
-
+                print("¡Correo editado con exito!")
             elif opc == "3":
                 print("********* ELIMINAR *********\n")
                 print("Por favor ingrese el codigo del estudiante a eliminar: ")
                 id = int(input())
                 del (dicEstudiantes[id])
+                print("¡Estudiante eliminado con exito!")
                 #dicEstudiantes[id]["nombre"] = input()
                 #opc = input()
             else:
@@ -153,17 +207,21 @@ while opcMenu != "0":
                 print("Por favor ingrese el nombre de la nueva materia: ")
                 nombre = input()
                 dicMaterias[id] = {"nombre": nombre}
+                print("¡Materia agregada con exito!")
             elif opc == "2":
                 print("********* EDITAR *********\n")
                 print("Por favor ingrese el código de la materia a editar: ")
                 id = int(input())
+                print("¡Codigo ingresado!")
                 print("Por favor ingrese el nuevo nombre de la materia: ")
                 dicMaterias[id]["nombre"] = input()
+                print("¡Materia editada con exito!")
             elif opc == "3":
                 print("******** ELIMINAR ********\n")
                 print("Por favor ingrese el código de la materia a eliminar: ")
                 id = int(input())
                 del (dicMaterias[id])
+                print("¡Materia eliminada con exito!")
                 #dicMaterias[id]["nombre"] = input()
                 #opc = input()
             else:
