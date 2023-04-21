0db9d7346d3650d1d98e595f3b07cea74242421e
Igmar De Jesus Lozada Bolivar <94941937+WingDark@users.noreply.github.com>
2023-04-20 21:50:36 -0500

    JSON

--- json.py
+++ json.py
@@ -1,8 +1,21 @@
 import json
 
-lista = [10, 20, 30, 40, 60]
+#Estructura de datos (Lista)
+#lista = [10, 20, 30, 40, 60]
+#diccionario = {'1':'Roy H. Llamas','2':'Igmar L. Bolivar','3':'David V. Duarte','4':'Andres E. Pajaro','5':'Daniel A. Rodriguez'}
 
-with open("./archivo/lista.json","w") as archivo:
-    json.dump(lista,archivo)
+#Fase de apertura y creacion
+with open('diccionario.json', 'r') as archivo:
+    #json.dump(diccionario,archivo)
+    diccionario = json.load(archivo)
 
+
+#Fase de cierroe
 archivo.close()
+
+print('\nDiccionario: ', diccionario)
+print(diccionario['1'])
+print(diccionario['2'])
+print(diccionario['3'])
+print(diccionario['4'])
+print(diccionario['5'])
